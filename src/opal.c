fe3d3aee830901cf6256a796469461649909dd15
Joshua <mckerracher@gmail.com>
2021-04-09 13:57:43 -0600

    Added libgen.h and changed the strategy used to get the include file's absolute path.

--- src/opal.c
+++ src/opal.c
@@ -6,6 +6,7 @@
 #include <string.h>             /* memset() */
 #include <errno.h>              /* errno macros and codes */
 #include <stdbool.h>            /* boolean datatypes */
+#include <libgen.h>             /* dirname() */
 
 #include "../include/opal.h"
 
@@ -338,13 +339,15 @@ proc_includes(FILE *source_fd, FILE *dest_fd)
                     logger(DEBUG, "Finished reading in the filename.");
 
                     ///Get the absolute path to the file before opening it.
-                    char curr_working_dir[256];
-                    getcwd(curr_working_dir, sizeof(curr_working_dir));
-                    strncat(curr_working_dir, filename_buffer, strlen(filename_buffer));
-                    logger(DEBUG, "Finished appending the filename to the current working directory");
+                    ///Get the absolute path to the file before opening it.
+                    char const *tmp = strdup(getenv( "PWD"));
+                    char *absolute_path = dirname(tmp);
+                    char *slash = "/";
+                    strncat(absolute_path, slash, 1);
+                    strncat(absolute_path, filename_buffer, strlen(filename_buffer));
 
                     ///Open file and get the first character in it.
-                    FILE *include_file = fopen(curr_working_dir, "r");
+                    FILE *include_file = fopen(absolute_path, "r");
                     char ch_2 = fgetc(include_file);
 
                     if (include_file != NULL)
