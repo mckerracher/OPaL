81902bd1c160795dc546359f8c789401f4b7b0cd
Joshua <mckerracher@gmail.com>
2021-04-14 16:58:11 -0600

    Made the edits recommended by Kedar.

--- test/test12.sh
+++ test/test12.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=build/
 build/marc --debug --output=test.tmp input/test2.opl
-if [[ $? -ne 2 ]] ; then
+if [[ $? -ne 0 ]] ; then
   exit 1
 fi
 exit 0
--- test/test15.sh
+++ test/test15.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=build/
 build/marc --invalid_flag --output=test.tmp input/test2.opl invalid_argument
-if [[ $? -ne 2 ]] ; then
+if [[ $? -ne 64 ]] ; then
   exit 1
 fi
 exit 0
