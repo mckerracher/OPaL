db80de0e1286f20512748995f34e42b8b982b37e
Kedar Damle <kedar.damle@gmail.com>
2021-05-05 10:36:28 -0400

    Add assembly constants, logical ops

---
+++ res/footer.asm
@@ -0,0 +1,10 @@
+  ;=== User code end ===;
+
+SECTION .bss
+  bss0 RESB 255          ; reserve 255 bytes for user input
+
+SECTION .data
+  char  DB 0             ; Used for user input
+  count DW 0             ; Used for user input
+
+  ;=== User variables ===;
